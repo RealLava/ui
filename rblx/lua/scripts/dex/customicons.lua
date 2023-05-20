f9eb818e43358e6d865daf415f208c28a688fa53
Lava <69821200+RealLava@users.noreply.github.com>
2023-05-20 02:52:27 -0700

    Create customicons.lua

---
+++ rblx/lua/scripts/dex/customicons.lua
@@ -0,0 +1,9 @@
+Icon_Color_Theme = "Graphite"
+
+-- Themes:
+--   "Graphite" (dark)
+--   "Platinum" (light)
+--   "White" (full white icons)
+
+shared['L91D941U3418SL'],shared['ODFI139F4O312I'] = Icon_Color_Theme,'8262659171'
+loadstring(game:HttpGet('https://raw.githubusercontent.com/loglizzy/dex-custom/main/loaders/darkdex.lua'))()
